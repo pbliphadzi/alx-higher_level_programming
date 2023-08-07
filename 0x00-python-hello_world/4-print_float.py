0f001e29c569b37ad23c976d0d7f9e417082f27a
pbliphadzi <122865634+pbliphadzi@users.noreply.github.com>
2023-08-07 19:31:17 +0000

    task 4 + 5

--- 0x00-python-hello_world/4-print_float.py
+++ 0x00-python-hello_world/4-print_float.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 number = 3.14159
-print(f"Float: {round(number, 2)}\n")
+round_float = round(number, 2)
+print(f"Float: {round_float}\n")
---
+++ 0x00-python-hello_world/5-print_string.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+str = "Holberton School"
+trip_str = 3 * str
+print(f"{trip_str}\n{str[:9]}")
