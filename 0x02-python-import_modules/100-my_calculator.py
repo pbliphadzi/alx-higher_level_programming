877b44c8d0a42f7f7f7e4f256fe635e54609370d
pbliphadzi <122865634+pbliphadzi@users.noreply.github.com>
2023-08-19 18:30:35 +0000

    0x02 4

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,28 +1,19 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-from sys import argv
-
-
-def calculate(a, operator, b):
-    if operator == "+":
-        print("{:d} {} {:d} = {:d}".format(a, operator, b, add(a, b)))
-    elif operator == "-":
-        print("{:d} {} {:d} = {:d}".format(a, operator, b, sub(a, b)))
-    elif operator == "*":
-        print("{:d} {} {:d} = {:d}".format(a, operator, b, mul(a, b)))
-    elif operator == "/":
-        print("{:d} {} {:d} = {:d}".format(a, operator, b, div(a, b)))
-    else:
-        print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
-
 
 if __name__ == "__main__":
-    if len(argv) != 4:
-        print("Usage: {} <a> <operator> <b>".format(argv[0]))
-        exit(1)
-    else:
-        a = int(argv[1])
-        operator = argv[2]
-        b = int(argv[3])
-        calculate(a, operator, b)
+    """Handle basic arithmetic operations."""
+    from calculator_1 import add, sub, mul, div
+    import sys
+
+    if len(sys.argv) - 1 != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+    if sys.argv[2] not in list(ops.keys()):
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
