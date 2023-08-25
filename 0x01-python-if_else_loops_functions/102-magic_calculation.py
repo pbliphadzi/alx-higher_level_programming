9789d65eca96c2149549c9596f0551d48e03b3eb
pbliphadzi <122865634+pbliphadzi@users.noreply.github.com>
2023-08-25 09:57:27 +0000

    fixing

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
-def magic_calculation(a, b):
-    return(98 + (a ** b))
+def magic_calculation(a, b, c):
+    if a < b and c > b:
+        return c
+    elif c > b:
+        return a + b
+    else:
+        return a * b - c
