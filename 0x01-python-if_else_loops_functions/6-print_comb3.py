ffcda61841d97ebb6a845810f3ae0e230ee1a4ea
pbliphadzi <122865634+pbliphadzi@users.noreply.github.com>
2023-08-25 08:56:14 +0000

    syntax correction

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 from itertools import combinations
 
 for digit1, digit2 in combinations(range(10), 2):
-    print(f"{digit1}{digit2}", end=", ")
+    print("{}{}".format(digit1, digit2), end=", ")
 
 print("89")
