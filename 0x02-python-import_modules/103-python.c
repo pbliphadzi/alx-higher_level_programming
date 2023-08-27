aadc31f2b79a03305f5295ab6e6b40ab86648b7d
pbliphadzi <122865634+pbliphadzi@users.noreply.github.com>
2023-08-27 17:58:04 +0000

    py.c

--- 0x02-python-import_modules/103-python.c
+++ 0x02-python-import_modules/103-python.c
@@ -14,25 +14,25 @@
  */
 void print_python_bytes(PyObject *p)
 {
-	long int size_of_bytes, i;
+    long int size_of_bytes, i;
 
-	printf("[.] bytes object info\n");
-	if (PyBytes_CheckExact(p))
-	{
-		size_of_bytes = PyBytes_Size(p);
-		printf("  size: %ld\n", size_of_bytes);
-		printf("  trying string: %s\n", PyBytes_AsString(p));
-		if (size_of_bytes >= 10)
-			size_of_bytes = 10;
-		else
-			size_of_bytes++;
-		printf("  first %ld bytes:", size_of_bytes);
-		for (i = 0; i < size_of_bytes; i++)
-			printf(" %02x", (int) PyBytes_AsString(p)[i] & 0xff);
-		printf("\n");
-	}
-	else
-		printf("  [ERROR] Invalid Bytes Object\n");
+    printf("[.] bytes object info\n");
+    if (PyBytes_CheckExact(p))
+    {
+        size_of_bytes = ((PyVarObject *)p)->ob_size; // Using ob_size instead of PyBytes_GET_SIZE
+        printf("  size: %ld\n", size_of_bytes);
+        printf("  trying string: %s\n", PyBytes_AsString(p));
+        if (size_of_bytes >= 10)
+            size_of_bytes = 10;
+        else
+            size_of_bytes++;
+        printf("  first %ld bytes:", size_of_bytes);
+        for (i = 0; i < size_of_bytes; i++)
+            printf(" %02x", (unsigned char) PyBytes_AsString(p)[i]);
+        printf("\n");
+    }
+    else
+        printf("  [ERROR] Invalid Bytes Object\n");
 }
 
 /**
@@ -46,20 +46,41 @@ void print_python_bytes(PyObject *p)
  */
 void print_python_list(PyObject *p)
 {
-	long int size_of_list = PyList_GET_SIZE(p), list_index;
-	PyObject *temp;
-	PyListObject *list_object = (PyListObject *) p;
+    long int size_of_list, list_index;
+    PyObject *temp;
+    PyObject *list_items = NULL;
+    PyObject *list_items_iterator = NULL;
 
-	printf("[*] Python list info\n");
-	printf("[*] Size of the Python List = %ld\n", size_of_list);
-	printf("[*] Allocated = %ld\n", list_object->allocated);
+    printf("[*] Python list info\n");
 
-	for (list_index = 0; list_index < size_of_list; list_index++)
-	{
-		temp = PyList_GET_ITEM(p, list_index);
-		printf("Element %ld: %s\n", list_index,
-				temp->ob_type->tp_name);
-		if (PyBytes_CheckExact(temp))
-			print_python_bytes(temp);
-	}
+    if (PyList_Check(p))
+    {
+        size_of_list = ((PyVarObject *)p)->ob_size; // Using ob_size instead of PyList_GET_SIZE
+        printf("[*] Size of the Python List = %ld\n", size_of_list);
+
+        list_items = PyObject_GetIter(p);
+        if (list_items != NULL)
+        {
+            list_index = 0;
+            while ((temp = PyIter_Next(list_items)) != NULL)
+            {
+                printf("Element %ld: %s\n", list_index, temp->ob_type->tp_name);
+                if (PyBytes_CheckExact(temp))
+                    print_python_bytes(temp);
+
+                Py_DECREF(temp);
+                list_index++;
+            }
+
+            Py_DECREF(list_items);
+        }
+        else
+        {
+            printf("[ERROR] Failed to retrieve list items.\n");
+        }
+    }
+    else
+    {
+        printf("  [ERROR] Invalid List Object\n");
+    }
 }
