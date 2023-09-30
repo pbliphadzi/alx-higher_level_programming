5d618362206199e8cf58a734b81bd399d5224543
pbliphadzi <122865634+pbliphadzi@users.noreply.github.com>
2023-09-30 18:30:12 +0000

    complete

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Prints Squares
+"""
+def print_square(size):
+    """
+    Write a function that prints a square with the character #.
+
+    """
+
+    if isinstance(size, float) and size < 0:
+        raise TypeError("size must be an integer")
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(size):
+        print("#" * size)
