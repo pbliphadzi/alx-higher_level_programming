052baa68066cf4889cbedc64ec1c5ff4802648a0
pbliphadzi <122865634+pbliphadzi@users.noreply.github.com>
2023-09-30 18:52:21 +0000

    try again

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -4,47 +4,68 @@
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
+
 class TestMaxInteger(unittest.TestCase):
-    """
-    Testing Son!!
-    """
+    """unittest class for max_integer"""
+    def test_module_docstring(self):
+        """Tests for module docsting"""
+        m = __import__('6-max_integer').__doc__
+        self.assertTrue(len(m) > 1)
 
-    def test_max_integer_no_argrument(self):
-        """Test if nothing is given"""
-        result = max_integer()
-        self.assertIsNone(result)
+    def test_function_docstring(self):
+        """Tests for funstion docstring"""
+        f = max_integer.__doc__
+        self.assertTrue(len(f) > 1)
 
-    def test_max_integer_empty_list(self):
-        """Test if nothing is given"""
-        result = max_integer([])
-        self.assertIsNone(result)
+    def test_empty_list(self):
+        """Tests for empty list []"""
+        e = []
+        self.assertIsNone(max_integer(e))
 
-    def test_max_integer_one_element_list(self):
-        """Test if nothing is given"""
-        result = max_integer([1])
-        self.assertEqual(result, 1)
+    def test_no_args(self):
+        """Tests for no arguments passed to func"""
+        self.assertIsNone(max_integer())
 
-    def test_max_integer_all_two_elements(self):
-        """Test if nothing is given"""
-        result = max_integer([2,3])
-        self.assertEqual(result, 3)
+    def test_one_element(self):
+        """Tests for only one number in the list"""
+        o = [1]
+        self.assertEqual(max_integer(o), 1)
 
-    def test_max_integer_all_the_same_elements(self):
-        """Test if nothing is given"""
-        result = max_integer([2,2,2,2])
-        self.assertEqual(result, 2)
+    def test_positive_end(self):
+        """Tests for all positive with max at end"""
+        e = [2, 10, 8, 36, 14, 50]
+        self.assertEqual(max_integer(e), 50)
 
-    def test_max_integer_all_example_one(self):
-        """Test if nothing is given"""
-        result = max_integer([1, 2, 3, 4])
-        self.assertEqual(result, 4)
+    def test_positive_middle(self):
+        """Tests for all positive with max in middle"""
+        m = [2, 10, 8, 360, 14, 50]
+        self.assertEqual(max_integer(m), 360)
 
-    def test_max_integer_all_example_two(self):
-        """Test if nothing is given"""
-        result = max_integer([1, 3, 4, 2])
-        self.assertEqual(result, 4)
+    def test_positive_beginning(self):
+        """Tests for all positive with max at beginning"""
+        b = [200, 10, 8, 36, 14, 50]
+        self.assertEqual(max_integer(b), 200)
 
-    def test_max_integer_error_raised(self):
-        """Test if nothing is given"""
-        with self.assertRaises(Exception):
-            max_integer(1)
+    def test_one_negative(self):
+        """Tests for list with one negative number"""
+        on = [200, 10, 8, -36, 14, 50]
+        self.assertEqual(max_integer(on), 200)
+
+    def test_all_negative(self):
+        """Tests for list with all negative numbers"""
+        n = [-6, -50, -75, -1, -100]
+        self.assertEqual(max_integer(n), -1)
+
+    def test_none(self):
+        """Tests for passing none as argument"""
+        with self.assertRaises(TypeError):
+            max_integer(None)
+
+    def test_non_int_arg(self):
+        """Tests for a non-int type in list"""
+        string = [1, 2, "Hello", 4, 5]
+        with self.assertRaises(TypeError):
+            max_integer(string)
+
+if __name__ == "__main__":
+    unittest.main()
