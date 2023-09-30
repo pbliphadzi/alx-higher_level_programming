13455e16d4a6dd577582f84787cfc6dba7c4efc5
pbliphadzi <122865634+pbliphadzi@users.noreply.github.com>
2023-09-30 18:49:19 +0000

    60-80

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,48 +1,14 @@
 #!/usr/bin/python3
-"""
-Module that multiplies 2 matrices
-"""
+# 101-lazy_matrix_mul.py
+"""Defines a matrix multiplication function using NumPy."""
 import numpy as np
+
+
 def lazy_matrix_mul(m_a, m_b):
-    """
-    Function that multiplies 2 matrices
+    """Return the multiplication of two matrices.
+    Args:
+        m_a (list of lists of ints/floats): The first matrix.
+        m_b (list of lists of ints/floats): The second matrix.
     """
 
-    if not isinstance(m_a, list):
-        raise TypeError("m_a must be a list")
-    if not isinstance(m_b, list):
-        raise TypeError("m_b must be a list")
-    if len(m_a) == 0:
-        raise ValueError("m_a can't be empty")
-    if len(m_b) == 0:
-        raise ValueError("m_b can't be empty")
-    for i in m_a:
-        if not isinstance(i, list):
-            raise TypeError("m_a must be a list of lists")
-        if len(i) == 0:
-            raise ValueError("m_a can't be empty")
-    for i in m_b:
-        if not isinstance(i, list):
-            raise TypeError("m_b must be a list of lists")
-        if len(i) == 0:
-            raise ValueError("m_b can't be empty")
-    size_a = len(m_a[0])
-    size_b = len(m_b[0])
-    for i in range(len(m_a)):
-        if len(m_a[i]) != size_a:
-            raise TypeError("each row of m_a must be of the same size")
-        for j in range(len(m_b[0])):
-            for k in range(len(m_b)):
-                if len(m_b[k]) != size_b:
-                    raise TypeError("each row of m_b must be of the same size")
-                if not isinstance(m_a[i][k], (int, float)):
-                    raise TypeError("m_a should contain only " +
-                                    "integers or floats")
-                if not isinstance(m_b[k][j], (int, float)):
-                    raise TypeError("m_b should contain only " +
-                                    "integers or floats")
-    try:
-        result = np.dot(m_a, m_b)
-    except Exception:
-        ValueError("m_a and m_b can't be multiplied")
-    return result
+    return (np.matmul(m_a, m_b))
